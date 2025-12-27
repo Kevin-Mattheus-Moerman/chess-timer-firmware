23ac4f42c7a452a6b0f34b992a6739baa806cfa2
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2025-12-27 11:03:31 +0000

    Updates to make chess timer

--- main.py
+++ main.py
@@ -17,14 +17,16 @@ from music_options import *
 # Hardware Configuration
 
 # Buttons are wired with pull-ups, so they read 1 when not pressed, 0 when pressed
-btn_up      = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
-btn_down    = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
-btn_confirm = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
+btn_increase      = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
+btn_decrease    = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
+btn_start_stop = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
 btn_reset   = machine.Pin(23, machine.Pin.IN, machine.Pin.PULL_UP)
 
 # NeoPixel LED on GPIO 1 (only 1 LED is used here)
 led = neopixel.NeoPixel(machine.Pin(1), 1)
-brightness_level = 0.3  # scale LED brightness so it’s not blinding
+brightness_level_base = 0.2  # scale LED brightness so it’s not blinding
+brightness_level_high = 0.3
+brightness_level_low = 0.1
 
 # Buzzer on GPIO 0 for playing melodies
 buzzer = machine.Pin(0, machine.Pin.OUT)
@@ -37,8 +39,8 @@ display = seven_segment.SevenSegmentDisplay()
 # Utility Functions
 
 def jingle_selection():
-    """Selects what jingle is played at the start, when study mode begins
-       and when rest mdoe begins. Jingles can be customized, see file music_options.py
+    """Selects what jingle is played at the start, when player_01 mode begins
+       and when player_02 mode begins. Jingles can be customized, see file music_options.py
        for more info
     """
     # Three jumpers select which jingle set to load
@@ -47,32 +49,38 @@ def jingle_selection():
     jumper3 = machine.Pin(18, machine.Pin.IN, machine.Pin.PULL_DOWN)
     
     # Globals store currently chosen jingles
-    global intro_jingle, work_jingle, rest_jingle
+    global intro_jingle, player_01_jingle, player_02_jingle, switch_jingle, start_jingle
     
-    # Each jumper corresponds to a different set of intro/work/rest melodies
+    # Each jumper corresponds to a different set of intro/player_01/rest melodies
     if jumper1.value()==1:
         intro_jingle = music(intro_jp1, pins=[buzzer], looping=False)
-        work_jingle = music(work_jp1, pins=[buzzer], looping=False)
-        rest_jingle = music(rest_jp1, pins=[buzzer], looping=False)
+        player_01_jingle = music(player_01_jp1, pins=[buzzer], looping=False)
+        player_02_jingle = music(player_02_jp1, pins=[buzzer], looping=False)
     elif jumper2.value()==1:
         intro_jingle = music(intro_jp2, pins=[buzzer], looping=False)
-        work_jingle = music(work_jp2, pins=[buzzer], looping=False)
-        rest_jingle = music(rest_jp2, pins=[buzzer], looping=False)
+        player_01_jingle = music(player_01_jp2, pins=[buzzer], looping=False)
+        player_02_jingle = music(player_02_jp2, pins=[buzzer], looping=False)
     elif jumper3.value()==1:
         intro_jingle = music(intro_jp3, pins=[buzzer], looping=False)
-        work_jingle = music(work_jp3, pins=[buzzer], looping=False)
-        rest_jingle = music(rest_jp3, pins=[buzzer], looping=False)
+        player_01_jingle = music(player_01_jp3, pins=[buzzer], looping=False)
+        player_02_jingle = music(player_02_jp3, pins=[buzzer], looping=False)
     else:
         # Default jingles if no jumper is set
         intro_jingle = music(intro, pins=[buzzer], looping=False)
-        work_jingle = music(work, pins=[buzzer], looping=False)
-        rest_jingle = music(rest, pins=[buzzer], looping=False)
+        player_01_jingle = music(player_01, pins=[buzzer], looping=False)
+        player_02_jingle = music(player_02, pins=[buzzer], looping=False)
         
+    switch_jingle = music(switch_sound, pins=[buzzer], looping=False) 
+    start_jingle = music(start_sound, pins=[buzzer], looping=False) 
 
-def play_jingle(jingle):
+
+def play_jingle(jingle, parallel_flag):
     """Starts a new thread that plays the jingle
     """
-    _thread.start_new_thread(_play_jingle_thread, (jingle,) )
+    if parallel_flag == True:
+        _thread.start_new_thread(_play_jingle_thread, (jingle,) )
+    else:
+        _play_jingle_thread(jingle)
 
 def _play_jingle_thread(jingle):
     """Plays the jingle given to it through an argument
@@ -84,37 +92,55 @@ def _play_jingle_thread(jingle):
         time.sleep(0.04)  # small delay for timing control
     jingle.restart()  # reset jingle so it can be played again later
 
-def update_led(color_tuple):
+def update_led(color_tuple, brightness_level):
     # Scale each color channel by brightness_level and send to NeoPixel
     led[0] = tuple(int(c * brightness_level) for c in color_tuple)
     led.write()
 
+def display_time(seconds):
+    # Convert amount in seconds into a MM:SS frame string
+    m = seconds // 60
+    s = seconds % 60
+    frame = f"{m:02}{s:02}" # always 4 chars (MMSS)
+    display.write(frame)
 
-def countdown(seconds, paused_flag, led_color_full, led_color_last_min=None):
+def countdown(seconds, paused_flag, led_color):
     """Counts down the seconds of a given mode and changes the 7-segment display
        to show it
     """
-    color_changed = False
+    start_seconds = seconds
+    led_low = False
+    done_flag = False
+    display.set_decimal_point(1, True) # Set middle decimal point on
+    while not done_flag and seconds >= 0:
+        # Change LED to last-minute color when only 60s left            
+        if seconds <= 10:
+            if led_low == True:
+                update_led(led_color, brightness_level_low)
+                led_low = False
+                # time.sleep_ms(200)
+            else:
+                update_led(led_color, brightness_level_high)
+                led_low = True
+                # time.sleep_ms(200)
 
-    while seconds >= 0:
-        # Change LED to last-minute color when only 60s left
-        if led_color_last_min and seconds == 60 and not color_changed:
-            update_led(led_color_last_min)
-            color_changed = True
-
-        # Convert remaining seconds into MM:SS frame string
-        m = seconds // 60
-        s = seconds % 60
-        frame = f"{m:02}{s:02}"   # always 4 chars (MMSS)
-        display.write(frame)
-
+        # Update displayed time
+        display_time(seconds)
+        
         # Align loop so each iteration lasts ~1 second
         end_time = time.ticks_add(time.ticks_ms(), 1000)
         while time.ticks_diff(end_time, time.ticks_ms()) > 0:
-            # Poll buttons during the wait
+            # Poll buttons etc during the wait
+                    
+            # Switch player toggle
+            if not btn_decrease.value():
+                time.sleep_ms(200)  # debounce delay
+                play_jingle(switch_jingle, False)
+                done_flag = True
+                break
 
             # Pause toggle
-            if not btn_confirm.value():
+            if not btn_start_stop.value():
                 time.sleep_ms(300)  # debounce delay
                 paused_flag[0] = not paused_flag[0]
                 
@@ -127,7 +153,7 @@ def countdown(seconds, paused_flag, led_color_full, led_color_last_min=None):
                 now = time.ticks_ms()
                 blink_on = (now // 500) % 2 == 0  # toggle every 500ms
 
-                if not btn_confirm.value():
+                if not btn_start_stop.value():
                     time.sleep_ms(300)
                     paused_flag[0] = False
                     break
@@ -138,83 +164,132 @@ def countdown(seconds, paused_flag, led_color_full, led_color_last_min=None):
                     display.clear()
 
                 time.sleep_ms(50)  # reduce busy-looping
-
-        seconds -= 1  # move to next second
-
+        if done_flag == False:
+            seconds -= 1  # move to next second
+            
+    display.clear() # Clear display
+    time_diff = start_seconds - seconds
+    if time_diff<=2:
+        return seconds + 2
+    else:
+        return seconds
+    
 
 def set_times():
-    # Default Pomodoro: 25 minutes study, 5 minutes rest
-    study = 25
-    rest = 5
-    idx = 0              # 0 = editing study, 1 = editing rest
-    debounce = 500       # debounce time in ms
+    # Default Chess timer: 
+    # 10 minutes game for each
+    time_edit_increment = 1
+    player_01 = 10
+    player_02 = 10
+    idx = 0              # 0 = editing player_01, 1 = editing player_02
+    debounce = 200       # debounce time in ms
     last = time.ticks_ms()
 
     while idx < 2:
         now = time.ticks_ms()
-        frame = f"{study:02}{rest:02}"  # Display both times side by side
+        frame = f"{player_01:02}{player_02:02}"  # Display both times side by side
         blink_on = (now // 500) % 2 == 0
 
         # Blink the currently edited field to show focus
         if (idx == 0 and not blink_on):
-            display.write("  " + frame[2:])  # hide study minutes
+            display.write("  " + frame[2:])  # hide player_01 minutes
         elif (idx == 1 and not blink_on):
-            display.write(frame[:2] + "  ")  # hide rest minutes
+            display.write(frame[:2] + "  ")  # hide player_02 minutes
         else:
             display.write(frame)
 
         # Button handling with debounce
-        if not btn_up.value() and time.ticks_diff(now, last) > debounce:
-            if idx == 0: study = min(study + 5, 95)  # step in 5 mins
-            else: rest = min(rest + 5, 95)
+        if not btn_increase.value() and time.ticks_diff(now, last) > debounce:
+            if idx == 0: 
+                player_01 = min(player_01 + time_edit_increment, 100-time_edit_increment)  # step in 5 mins
+            else: 
+                player_02 = min(player_02 + time_edit_increment, 100-time_edit_increment)
             last = now
 
-        if not btn_down.value() and time.ticks_diff(now, last) > debounce:
-            if idx == 0: study = max(study - 5, 0)
-            else: rest = max(rest - 5, 0)
+        if not btn_decrease.value() and time.ticks_diff(now, last) > debounce:
+            if idx == 0: 
+                player_01 = max(player_01 - time_edit_increment, 0)
+            else: 
+                player_02 = max(player_02 - time_edit_increment, 0)
             last = now
 
-        if not btn_confirm.value() and time.ticks_diff(now, last) > debounce:
+        if not btn_start_stop.value() and time.ticks_diff(now, last) > debounce:
             idx += 1  # move to next field
             last = now
 
         time.sleep_ms(40)  # small delay to avoid busy-looping
 
-    return study * 60, rest * 60  # return values in seconds
+    return player_01 * 60, player_02 * 60  # return values in seconds
 
+def stream_text(frame, time_delay, number_of_repeats):
+    n = len(frame)
+    # print("n = ", n)
+    for _ in range(0, number_of_repeats):
+        for i in range(0, n-3): # 0 - n-4            
+            display.write(frame[i:i+4])
+            time.sleep_ms(time_delay) 
 
 # ──────────────────────────────────────────────
 # Main Loop
 
 def main():
-    update_led((35, 91, 121))  # Initial LED color = Soldered purple
+    while True: 
+        # Led colors
+        update_led((35, 91, 121), brightness_level_base)  # Initial LED color = Soldered purple
+
+        # Sounds
+        jingle_selection()          # Choose jingles based on jumpers
+        play_jingle(intro_jingle, True)  # Play intro tune
+
+        frame = f"_-~-_-~ CHESS ~-_-~-"   # always 4 chars (MMSS)
+        stream_text(frame, 200, 1)
+
+        color_player_01 = (255,  255,   0) # Color for player 1
+        color_player_02 = (  0,    0, 255)  # Color for player 2
+        color_players = (color_player_01, color_player_02)
+        
+        # Timing
+        player_01_secs, player_02_secs = set_times()  # let user adjust times
+        paused_flag = [False]  # list used so value can be mutated inside functions
+
+        # Play short countdown sound to indicate ready/set/go
+        frame = f"GET READY"   # always 4 chars (MMSS)
+        stream_text(frame, 200, 1)
+        display.write(f"----")
+
+        # Display first player time
+        update_led(color_players[0], brightness_level_base) # LED color for player 1
+        play_jingle(start_jingle, False)
+
+        print("Starting game.")
+        while True: 
+            # Player 1
+            update_led(color_players[0], brightness_level_base) # LED color for player 1
+            player_01_secs = countdown(player_01_secs, paused_flag, color_players[0])
+
+            if player_01_secs <= 0:
+                update_led(color_players[1], brightness_level_base) # LED color for player 2
+                play_jingle(player_02_jingle, False)
+                frame = f"PLAYER 2 SITH"   
+                stream_text(frame, 200, 1)
+                time.sleep(1.0)
+                break
+            
+            # Player 2 
+            update_led(color_players[1], brightness_level_base) # LED color for player 2
+            player_02_secs = countdown(player_02_secs, paused_flag, color_players[1])
+
+            if player_02_secs <= 0:
+                update_led(color_players[0], brightness_level_base) # LED color for player 1
+                play_jingle(player_01_jingle, False)
+                frame = f"PLAYER 1 JEDI"   
+                stream_text(frame, 200, 1)
+                time.sleep(1.0)
+                break
+
+            time.sleep_ms(50)  # small delay to avoid busy-looping 
     
-    jingle_selection()          # Choose jingles based on jumpers
-    play_jingle(intro_jingle)  # Play intro tune
-
-    study_secs, rest_secs = set_times()  # let user adjust times
-    paused = [False]  # list used so value can be mutated inside functions
-
-    while True:
-        # Work session
-        update_led((0, 255, 0))  # LED = red during study
-        play_jingle(work_jingle)
-        time.sleep(0.3)
-        countdown(
-            study_secs, paused,
-            led_color_full=(0, 255, 0),        # Red during study
-            led_color_last_min=(255, 255, 0)   # Yellow last minute
-        )
-
-        # Rest session
-        update_led((255, 0, 0))  # LED = Green during rest
-        play_jingle(rest_jingle)
-        countdown(
-            rest_secs, paused,
-            led_color_full=(255, 0, 0),        # Green during rest
-            led_color_last_min=(255, 255, 0)   # Yellow last minute
-        )
-
+        
 
 if __name__ == "__main__":
     main()  # Run program only if file is executed directly
--- music_options.py
+++ music_options.py
@@ -40,21 +40,23 @@ This plays C4 for 2 beats, E4 for 2 beats starting at beat 2, and G4 for 4 beats
 
 # Default musical sequences (played when no jumpers are connected)
 # Create your own at OnlineSequencer.net and paste the exported Arduino code here
-intro = '2 A6 3 17;6 E6 1 17;9 G6 4 17;0 E6 1 17'  # Introductory music sequence
-work = '2 B4 2 43;0 E5 1 43;4 B4 1 43;5 C5 1 43;6 C#5 1 43'  # Work period music
-rest = '9 G#6 1 17;0 D#6 1 17;3 F6 1 17;5 G#6 1 17;7 A#6 1 17'  # Rest period music
+intro = '16 E4 1 41;24 A4 1 41;17 F#4 1 41;18 G4 1 41;20 A4 1 41;22 C4 1 41;26 G4 1 41;28 E4 1 41;27 F#4 1 41;0 G3 1 41;2 A3 1 41;4 G3 1 41;5 F#3 1 41;6 E3 1 41;8 G3 1 41;10 A3 1 41;12 G3 1 41;13 F#3 1 41;14 E3 1 41'#'0 E5 1 14;1 E5 1 14;3 E5 1 14;6 E5 1 14;5 C5 1 14;8 G5 1 14'#'2 A6 3 17;6 E6 1 17;9 G6 4 17;0 E6 1 17'  # Introductory music sequence
+player_01 = '0 D4 4 50;4 G4 8 50;12 A4 6 50;18 A#4 1 50;19 C5 1 50;20 A#4 8 50;28 D4 6 50'#'2 B4 2 43;0 E5 1 43;4 B4 1 43;5 C5 1 43;6 C#5 1 43'  # Player 1 music
+player_02 = '15 A#4 1 50;15 A#4 1 50;15 A#4 1 51;24 G4 2 51;16 G4 2 51;15 A#4 1 43;0 G4 3 43;4 G4 3 43;8 G4 3 43;12 D#4 2 43;16 G4 2 43;24 G4 4 43;15 A#3 1 43;20 D#4 2 43;23 A#4 1 43;23 A#3 1 43' # '9 G#6 1 17;0 D#6 1 17;3 F6 1 17;5 G#6 1 17;7 A#6 1 17'  # Player 2 music
+switch_sound = '0 A6 1 43'#'3 A6 2 43;0 C7 2 43' 
+start_sound = '0 C7 1 43;16 C7 1 43;8 C7 1 43;24 B7 3 43'
 
 # Alternative musical sequences for jumper 1 (JP1)
 intro_jp1 = '0 F#6 1 21;1 A6 1 21;3 C7 1 21;5 B6 1 21'
-work_jp1 = '7 G#4 1 21;0 F4 1 21;2 F#4 1 21;4 G#4 1 21;6 A#4 1 21'
-rest_jp1 = '6 A7 2 43;0 A6 2 43;3 C#7 2 43'
+player_01_jp1 = '7 G#4 1 21;0 F4 1 21;2 F#4 1 21;4 G#4 1 21;6 A#4 1 21'
+player_02_jp1 = '6 A7 2 43;0 A6 2 43;3 C#7 2 43'
 
 # Alternative musical sequences for jumper 2 (JP2)
 intro_jp2 = '8 G#6 2 43;0 G#6 2 43;3 B6 2 43;6 D#7 2 43;11 A#6 2 43'
-work_jp2 = '0 C#7 1 41;3 E7 1 41;5 F#7 1 41;9 G#7 1 41'
-rest_jp2 = '0 C#7 2 41;4 G#7 2 41;2 E7 2 41'
+player_01_jp2 = '0 C#7 1 41;3 E7 1 41;5 F#7 1 41;9 G#7 1 41'
+player_02_jp2 = '0 C#7 2 41;4 G#7 2 41;2 E7 2 41'
 
 # Alternative musical sequences for jumper 3 (JP3)
 intro_jp3 = '4 A#6 3 25;0 F6 1 25;2 G6 1 25'
-work_jp3 = '9 D6 3 25;0 B6 3 25;3 G6 3 25;6 C6 3 25'
-rest_jp3 = '10 F7 3 25;0 G6 3 25;4 A#6 3 25;7 D7 3 25'
+player_01_jp3 = '9 D6 3 25;0 B6 3 25;3 G6 3 25;6 C6 3 25'
+player_02_jp3 = '10 F7 3 25;0 G6 3 25;4 A#6 3 25;7 D7 3 25'
